004b8606b8310087b726452eda0321b4def056cd
Nick Cao <nickcao@nichi.co>
2022-05-31 19:49:59 +0800

    fix build with nix 2.9

--- canopus.cc
+++ canopus.cc
@@ -6,9 +6,9 @@
 #include <sys/prctl.h>
 #include <sys/syscall.h>
 #include <iostream>
+#include "nix/callback.hh"
 #include "nix/eval-inline.hh"
 #include "nix/store-api.hh"
-#include "nix/callback.hh"
 
 struct DummyStoreConfig : virtual nix::StoreConfig {
   using nix::StoreConfig::StoreConfig;
@@ -92,144 +92,24 @@ struct Evaluator {
 
   std::string eval(std::string expr) {
     nix::Value value;
-    std::set<nix::Value*> seen;
-    std::ostringstream out;
     state->eval(state->parseExprFromString(expr, "/var/empty"), value);
-    format_value(out, value, 1, seen);
-    return out.str();
-  }
-
-  static bool check_varname(std::string_view str) {
-    if (str.size() == 0)
-      return false;
-    char chr = str[0];
-    if ((chr >= '0' && chr <= '9') || chr == '-' || chr == '\'')
-      return false;
-    for (auto& i : str)
-      if (!((i >= 'a' && i <= 'z') || (i >= 'A' && i <= 'Z') ||
-            (i >= '0' && i <= '9') || i == '_' || i == '-' || i == '\''))
-        return false;
-    return true;
-  }
-
-  static std::ostream& format_string(std::ostream& out, const char* str) {
-    out << "\"";
-    for (const char* i = str; *i; i++)
-      if (*i == '\"' || *i == '\\')
-        out << "\\" << *i;
-      else if (*i == '\n')
-        out << "\\n";
-      else if (*i == '\r')
-        out << "\\r";
-      else if (*i == '\t')
-        out << "\\t";
-      else
-        out << *i;
-    out << "\"";
-    return out;
-  }
-
-  std::ostream& format_value(std::ostream& out,
-                             nix::Value& value,
-                             uint64_t depth,
-                             std::set<nix::Value*>& seen) {
-    state->forceValue(value, [&]() { return value.determinePos(nix::noPos); });
-    switch (value.type()) {
-      case nix::nInt:
-        out << value.integer;
-        break;
-      case nix::nBool:
-        out << (value.boolean ? "true" : "false");
-        break;
-      case nix::nString:
-        format_string(out, value.string.s);
-        break;
-      case nix::nPath:
-        out << value.path;
-        break;
-      case nix::nNull:
-        out << "null";
-        break;
-      case nix::nAttrs: {
-        seen.insert(&value);
-        bool isDrv = state->isDerivation(value);
-        if (isDrv) {
-          out << "«derivation ";
-          nix::Bindings::iterator i = value.attrs->find(state->sDrvPath);
-          nix::PathSet context;
-          if (i != value.attrs->end())
-            out << state->store->printStorePath(
-                state->coerceToStorePath(*i->pos, *i->value, context));
-          else
-            out << "???";
-          out << "»";
-        } else if (depth > 0) {
-          out << "{ ";
-          typedef std::map<std::string, nix::Value*> Sorted;
-          Sorted sorted;
-          for (auto& i : *value.attrs)
-            sorted[i.name] = i.value;
-          for (auto& i : sorted) {
-            if (check_varname(i.first))
-              out << i.first;
-            else
-              format_string(out, i.first.c_str());
-            out << " = ";
-            if (seen.count(i.second))
-              out << "«repeated»";
-            else
-              try {
-                format_value(out, *i.second, depth - 1, seen);
-              } catch (nix::AssertionError& e) {
-                out << "«error: " << e.msg() << "»";
-              }
-            out << "; ";
-          }
-          out << "}";
-        } else
-          out << "{ ... }";
-        break;
+    state->forceValue(value, nix::noPos);
+    if (state->isDerivation(value)) {
+      auto i = value.attrs->find(state->sDrvPath);
+      nix::PathSet context;
+      std::ostringstream ss;
+      ss << "<derivation ";
+      if (i != value.attrs->end()) {
+        ss << state->store->printStorePath(
+            state->coerceToStorePath(i->pos, *i->value, context));
+      } else {
+        ss << "???";
       }
-      case nix::nList:
-        seen.insert(&value);
-        out << "[ ";
-        if (depth > 0)
-          for (auto elem : value.listItems()) {
-            if (seen.count(elem))
-              out << "«repeated»";
-            else
-              try {
-                format_value(out, *elem, depth - 1, seen);
-              } catch (nix::AssertionError& e) {
-                out << "«error: " << e.msg() << "»";
-              }
-            out << " ";
-          }
-        else
-          out << "... ";
-        out << "]";
-        break;
-      case nix::nFunction:
-        if (value.isLambda()) {
-          std::ostringstream s;
-          s << value.lambda.fun->pos;
-          out << "«lambda @ " << nix::filterANSIEscapes(s.str(), true) << "»";
-        } else if (value.isPrimOp()) {
-          out << "«primop»";
-        } else if (value.isPrimOpApp()) {
-          out << "«primop-app»";
-        } else {
-          throw std::invalid_argument("unrecognized function");
-        }
-        break;
-      case nix::nFloat:
-        out << value.fpoint;
-        break;
-      default:
-        out << "«unknown»";
-        break;
+      ss << ">";
+      return ss.str();
+    } else {
+      return nix::printValue(*state, value);
     }
-    return out;
   }
 
   static void init() {
